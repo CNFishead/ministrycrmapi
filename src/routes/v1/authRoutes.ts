7c0648859ecf7bce76cdcc50d1ef03318dbb5fa9
Austin Howard <cnfishead@hotmail.com>
2024-11-10 20:00:23 -0500

    recaptcha

--- src/routes/v1/authRoutes.ts
+++ src/routes/v1/authRoutes.ts
@@ -5,12 +5,14 @@ import forgotpassword from "../../controllers/auth/forgotpassword";
 import checkUsername from "../../controllers/auth/checkUsername";
 import checkEmailExists from "../../controllers/auth/checkEmailExists";
 import getMe from "../../controllers/auth/getMe";
+import recaptcha from "../../controllers/auth/recaptcha";
 const router = express.Router();
 
 // Import all of our routes
 router.route("/:username/username").get(checkUsername);
 router.route("/:email/email").get(checkEmailExists);
 router.route("/register").post(register);
+router.route("/recaptcha").post(recaptcha);
 router.route("/forgot-password").post(forgotpassword);
 router.route("/login").post(login);
 router.route("/me").post(getMe);
